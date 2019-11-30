7a18cd293d72730568e7d5173ccfd73f34bcc1c9
neetesshhr <rijalboy.94@gmail.com>
2019-11-30 11:02:39 +0545

    UI made nice looking

--- login.php
+++ login.php
@@ -11,25 +11,29 @@
     <?php
      include 'bootstrap.php';
     ?>
-    <h1 class="text-center" style="font-family: 'Solway', serif;"> Login  </h1>
-       <div class="container" style="font-family: 'Solway', serif;">
+    <h1 class="text-center" style="font-family: 'Solway', serif; color: #5822a3;"> Login  </h1>
+       <div class="container col-md-4 text-center" style="font-family: 'Solway', serif;">
          <form action="" method="post">
-         <div class="form-group">
+         <div class="form-group" style='color: #0dd61a;'>
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your E-mail">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your E-mail" required >
    
   </div>
-  <div class="form-group">
+  <div class="form-group"  style='color: #0dd61a;'>
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"placeholder="Your password" >
+    <input type="password" class="form-control" id="exampleInputPassword1"placeholder="Your password" required >
   </div>
   
-  <button type="submit" class="btn btn-primary">Login</button> <br>
-  <label> New Here <br>
-  <a class="btn btn-primary text-center" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  <button type="submit"  id="singlebutton" name="singlebutton" class="btn btn-primary">Login</button> 
+     
+     <br>
+     <h5 style="color: #2d8043;">OR!</h5>
+  
+  <a class="btn btn-primary center-block "  data-toggle="collapse" href="register.php" role="button" aria-expanded="false" aria-controls="collapseExample">
    Register
   </a>
-  </label>
+  
+   
          </form>
        </div>
 </body>
