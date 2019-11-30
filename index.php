37f97010ec522c2c4c0060bb981dc76e7683bc44
kamal <mraryalkamal2011@gmail.com>
2019-11-30 11:53:09 +0545

    index post almost completed

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+session_start();
+if (isset($_SESSION['uid_session'])) {
+  header("location: donate/afterlogin.php");
+}else{
+}
+  ?>
+
+  
+   <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+  </head>
+  <body>
+   <?php
+   include 'bootstrap.php';
+   include 'navbarindex.php';
+   ?>
+  <!-- <?php
+      
+       include "slidingimages.php";
+       ?> -->
+    
+        <div class="container">
+        <br>
+        <br>
+        <br>
+        <?php
+         include 'posts.php';
+         ?>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+       <!-- footer -->
+       <footer id="main-footer" class="bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center py-4">
+                    <h3 class="text-light">No waste Food</h3>
+                    <p class="text-light">Copyright &copy; 2019 <span id="year"></span></p>
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#contactmodal">Contact Us</button>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+</body>
+</html>
