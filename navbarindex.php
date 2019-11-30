f4e2ba7d697b4e0928affde0d9ed9fd8aa6ed4b3
kamal <mraryalkamal2011@gmail.com>
2019-11-30 11:06:04 +0545

    navbarindex kaam finished

--- navbarindex.php
+++ navbarindex.php
@@ -9,6 +9,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
 </head>
 <body>
 <div>
@@ -24,8 +28,8 @@
 
 
                 <a class="nav-item nav-link text-light" href="index.php">Home</a> &nbsp &nbsp &nbsp &nbsp
-                <a class="nav-item nav-link text-light" href="#">Explore</a> &nbsp &nbsp &nbsp &nbsp
-                <a class="nav-item nav-link text-light" href="#">Share</a>
+                <a class="nav-item nav-link text-light" href="feedpeople.php">Explore</a> &nbsp &nbsp &nbsp &nbsp
+                <a class="nav-item nav-link text-light" href="contact.php">Share</a>
 
 
             </div>
@@ -33,12 +37,14 @@
         <form class="form-inline">
             <div class="container"> 
 
-                <button class="btn btn-lg btn-outline-success"><a href="login.php" class="text-success card-link text-light">Login</a></button>
-                <button class="btn btn-lg btn-outline-success"><a href="register.php" class="text-success card-link text-light">Register</a></button>
+                <button class="btn btn-md btn-outline-success"><a href="login.php" class="text-success card-link text-light">Login</a></button>
+                <button class="btn btn-md btn-outline-success"><a href="register.php" class="text-success card-link text-light">Register</a></button>
 
             </div>
         </form>
+      
     </nav>
+    <hr class="new5">
     </div>
 </body>
 </html>
